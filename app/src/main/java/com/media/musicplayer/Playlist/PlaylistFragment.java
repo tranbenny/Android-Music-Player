9f47644a111f90de7316559a234a1bb56f048143
tranbenny <tran.benny123@gmail.com>
2016-04-30 18:20:23 -0700

    initial commit and created project layout

---
+++ app/src/main/java/com/media/musicplayer/Playlist/PlaylistFragment.java
@@ -0,0 +1,31 @@
+package com.media.musicplayer.Playlist;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.media.musicplayer.R;
+
+
+public class PlaylistFragment extends Fragment {
+
+    public PlaylistFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.playlist_fragment, container, false);
+        return view;
+    }
+
+
+}
